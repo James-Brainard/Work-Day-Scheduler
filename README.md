9ee1d27a396000bc12930a2d811e18c10f74a8a4
James Brainard <james.brainard2@aol.com>
2023-08-31 20:44:47 -0700

    removed develop folder, completed readme, added descriptive comments to js file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Work Day Scheduler Starter Code
+
+## Description
+
+A work day scheduler application to keep track of your daily tasks during the typical work day hours.
+
+# Installtion
+
+N/A
+
+## Usage
+
+To dynamically keep track of your daily tasks. Whether it's for work or personal.
+
+## License
+
+N/A
+
+## Technologies
+
+HTML, CSS, Javascript & Jquery
+
+## 
+
+https://james-brainard.github.io/Work-Day-Scheduler/
+
+## Screenshot of Application
+
+[Application Screenshot](Work-Scheduler.JPG)
---
+++ assets/script.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function () {
+  let currentTimeDay = dayjs();
+  let hour = dayjs().format('HH');
+  // Logic to determine if current time block is in the past, present or future.
+  $('.container-fluid').find('.py-3').each( function (){
+    let timeBlock = $(this).data("time");
+    if (timeBlock == hour) {
+      $(this).parent().removeClass('past present future').addClass("present");
+    }
+    if (timeBlock < hour) {
+      $(this).parent().removeClass('past present future').addClass("past");
+    }
+    if (timeBlock > hour) {
+      $(this).parent().removeClass('past present future').addClass("future");
+    }
+  })
+  // Logic input the current date to the header in the format below.
+  $('#currentDay').text(currentTimeDay.format('MMM D, YYYY'));
+
+  // Logic to understand which save button had an input saved to its textarea.
+  let saveBtn = $('.saveBtn');
+  saveBtn.on('click', function () {
+    let input = $(this).siblings(".description").val();
+    let key = $(this).parent().attr('id');
+    localStorage.setItem(key, input);
+    console.log(key, input);
+  });
+  
+  // Logic to save the scheduled task after a page refresh. 
+  $('.time-block').each( function (){
+    let value = $(this).attr('id');
+    let storedValue = localStorage.getItem(value);
+    if (storedValue !== null) {
+      $(this).children('.description').val(storedValue);
+    }
+    
+})});
